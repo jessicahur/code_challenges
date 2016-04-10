379ef514a6c4d9a5f8aad4f97315573a61b9414b
Jessica Tran Hur <jesshur824@gmail.com>
2016-04-10 16:55:55 -0500

    TDD for number palindrome

---
+++ number_palindrome/palindrome.js
@@ -0,0 +1,35 @@
+function main (num) {
+  var length = num.length;
+  if (length === 0) {
+    return false;
+  }
+  else if(length === 1 ) {
+    return true;
+  }
+  else {
+    //even length number
+    if (length % 2 === 0) {
+      var midNum = length / 2;
+      var firstHalf = num.substr(0, midNum);
+      var secondHalf = helper(num.substr(midNum, midNum));
+    }
+    //odd length number
+    else {
+      var midNum = Math.ceil(length / 2);
+      var firstHalf = num.substr(0, midNum - 1);
+      var secondHalf = helper(num.substr(midNum, midNum));
+    }
+    if (firstHalf === secondHalf) return true;
+    else return false;
+  }
+};
+
+function helper (str) {
+  var result = '';
+  for (var ii = str.length - 1; ii >= 0; ii --) {
+    result += str[ii];
+  }
+  return result;
+}
+
+module.exports = main;
---
+++ number_palindrome/test/palindrome.test.js
@@ -0,0 +1,23 @@
+var assert = require('chai').assert;
+var main = require( '../palindrome');
+
+describe('Palindrome for numbers', () => {
+  it ('should return true for the case of single digit and false if input is empty', () => {
+    assert.equal(main('1'), true);
+    assert.equal(main(''), false);
+  });
+
+  it ('should return true for even length number palindrome', () => {
+    assert.equal(main('22'), true, 'it fails at 22');
+    assert.equal(main('9229'), true, 'it fails at 9229');
+  });
+
+  it ('should return true for odd length number palindrome', () => {
+    assert.equal(main('12321'), true, 'it fails at 12321');
+  });
+
+  it('should return false if input number is not palindrome', () => {
+    assert.equal(main('12'), false, 'fails at 12');
+    assert.equal(main('123'), false, 'fails at 123');
+  })
+});
