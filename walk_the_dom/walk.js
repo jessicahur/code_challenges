42758e6849696f07b80e73e6431c61296973c10f
Jessica Tran Hur <jesshur824@gmail.com>
2016-04-12 14:22:57 -0500

    utilize walk_the_DOM to get all DOM elements that matches an attribute

--- walk_the_dom/walk.js
+++ walk_the_dom/walk.js
@@ -17,3 +17,39 @@ For each node:
     * yes: call walk on this firstChild -> recursive | Once done, assign node on the next sibling
     * no: terminate while loop and the function stops
 */
+
+// Define a getElementsByAttribute function. It
+// takes an attribute name string and an optional
+// matching value. It calls walk_the_DOM, passing it a
+// function that looks for an attribute name in the
+// node. The matching nodes are accumulated in a
+// results array.
+
+var getElementsByAttribute = function (attribute, value) {
+  var results = [];
+
+  walk_the_DOM(document.body, function (node) {
+    //We're only interested in element nodes which match the input attribute
+    //The value of actual will be the element node's attribute's value
+    var actual = node.nodeType === 1 && node.getAttribute(attribute);
+
+    //Use typeof value !== 'string' because value is optional, can be undefined, and we don't accept any other value type other than string.
+    if (typeof actual === 'string' &&
+                (actual === value || typeof value !== 'string')) {
+      results.push(node);
+    }
+  });
+  return results;
+};
+
+/* Notes:
+For nodeType:
+  If the node is an element node, the nodeType property will return 1.
+
+  If the node is an attribute node, the nodeType property will return 2.
+
+  If the node is a text node, the nodeType property will return 3.
+
+  If the node is a comment node, the nodeType property will return 8.
+
+*/
